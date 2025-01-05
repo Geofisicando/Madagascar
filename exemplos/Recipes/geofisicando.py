d1b627a7b5d8d666c708e0244b86c3a459ea231b
Dirack <rodolfo_profissional@hotmail.com>
2025-01-05 15:23:02 -0300

    Exemplo recipes do Madagascar

---
+++ exemplos/Recipes/geofisicando.py
@@ -0,0 +1,9 @@
+from rsf.proj import *
+
+def velplot(name):
+	Plot(name,
+     	'''
+     	grey color=j allpos=y bias=1.5 scalebar=y wanttitle=n
+     	barreverse=y barlabel=Velocity barunit=km/s
+     	''')
+
